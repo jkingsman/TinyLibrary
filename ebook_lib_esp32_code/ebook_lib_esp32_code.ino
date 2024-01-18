ba6c29494f79c6e3963a1657f2cda99c55d36cdb
jkingsman <jack.kingsman@gmail.com>
2024-01-18 10:37:46 -0800

    Formatting

--- ebook_lib_esp32_code/ebook_lib_esp32_code.ino
+++ ebook_lib_esp32_code/ebook_lib_esp32_code.ino
@@ -20,12 +20,11 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
-
 #include <Arduino.h>
 
 // Replace with your network credentials
-const char* ssid = "Free eBooks -- Connect to Read";
-const char* password;
+const char *ssid = "Free eBooks -- Connect to Read";
+const char *password;
 
 // set addressing
 const IPAddress localIP(192, 168, 4, 1);
@@ -36,14 +35,17 @@ const String localIPURL = "http://192.168.4.1/";
 AsyncWebServer server(80);
 DNSServer dnsServer;
 
-void initSDCard(){
-  if(!SD.begin()){
+void initSDCard()
+{
+  if (!SD.begin())
+  {
     Serial.println("Card Mount Failed");
     return;
   }
   uint8_t cardType = SD.cardType();
 
-  if(cardType == CARD_NONE){
+  if (cardType == CARD_NONE)
+  {
     Serial.println("No SD card attached");
     return;
   }
@@ -52,54 +54,63 @@ void initSDCard(){
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
 }
 
-void initWiFi() {
+void initWiFi()
+{
   WiFi.mode(WIFI_MODE_AP);
   WiFi.softAPConfig(localIP, gatewayIP, subnetMask);
   WiFi.softAP(ssid, password);
 
   // Disable AMPDU RX on the ESP32 WiFi to fix a bug on Android per github.com/CDFER/Captive-Portal-ESP32
-	esp_wifi_stop();
-	esp_wifi_deinit();
-	wifi_init_config_t my_config = WIFI_INIT_CONFIG_DEFAULT();
-	my_config.ampdu_rx_enable = false;
-	esp_wifi_init(&my_config);
-	esp_wifi_start();
-	vTaskDelay(100 / portTICK_PERIOD_MS);  // Add a small delay
+  esp_wifi_stop();
+  esp_wifi_deinit();
+  wifi_init_config_t my_config = WIFI_INIT_CONFIG_DEFAULT();
+  my_config.ampdu_rx_enable = false;
+  esp_wifi_init(&my_config);
+  esp_wifi_start();
+  vTaskDelay(100 / portTICK_PERIOD_MS); // Add a small delay
   Serial.print("Hosting WIFI AP '");
   Serial.print(ssid);
   Serial.print("' with gateway at ");
   Serial.println(localIPURL);
 }
 
-void setUpDNSServer() {
-	// Set the TTL for DNS response and start the DNS server
-	dnsServer.setTTL(3600);
-	dnsServer.start(53, "*", localIP);
+void setUpDNSServer()
+{
+  // Set the TTL for DNS response and start the DNS server
+  dnsServer.setTTL(3600);
+  dnsServer.start(53, "*", localIP);
   Serial.println("DNS server is up");
 }
 
-void setUpWebServer() {
-  server.on("/connecttest.txt", [](AsyncWebServerRequest *request) { request->redirect("http://logout.net"); });	// windows 11 captive portal workaround
-  server.on("/wpad.dat", [](AsyncWebServerRequest *request) { request->send(404); });								// Honestly don't understand what this is but a 404 stops win 10 keep calling this repeatedly and panicking the esp32 :)
+void setUpWebServer()
+{
+  server.on("/connecttest.txt", [](AsyncWebServerRequest *request)
+            { request->redirect("http://logout.net"); }); // windows 11 captive portal workaround
+  server.on("/wpad.dat", [](AsyncWebServerRequest *request)
+            { request->send(404); }); // Honestly don't understand what this is but a 404 stops win 10 keep calling this repeatedly and panicking the esp32 :)
 
   // github.com/CDFER/Captive-Portal-ESP32
   // Background responses: Probably not all are Required, but some are. Others might speed things up?
   // A Tier (commonly used by modern systems)
-  server.on("/generate_204", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });		   // android captive portal redirect
-  server.on("/redirect", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });			   // microsoft redirect
-  server.on("/hotspot-detect.html", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });  // apple call home
-  server.on("/canonical.html", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });	   // firefox captive portal call home
-  server.on("/success.txt", [](AsyncWebServerRequest *request) { request->send(200); });					   // firefox captive portal call home
-  server.on("/ncsi.txt", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });			   // windows call home
+  server.on("/generate_204", [](AsyncWebServerRequest *request)
+            { request->redirect(localIPURL); }); // android captive portal redirect
+  server.on("/redirect", [](AsyncWebServerRequest *request)
+            { request->redirect(localIPURL); }); // microsoft redirect
+  server.on("/hotspot-detect.html", [](AsyncWebServerRequest *request)
+            { request->redirect(localIPURL); }); // apple call home
+  server.on("/canonical.html", [](AsyncWebServerRequest *request)
+            { request->redirect(localIPURL); }); // firefox captive portal call home
+  server.on("/success.txt", [](AsyncWebServerRequest *request)
+            { request->send(200); }); // firefox captive portal call home
+  server.on("/ncsi.txt", [](AsyncWebServerRequest *request)
+            { request->redirect(localIPURL); }); // windows call home
 
   // 404 handler
-	server.onNotFound([](AsyncWebServerRequest *request) {
-		request->redirect(localIPURL);
-	});
+  server.onNotFound([](AsyncWebServerRequest *request)
+                    { request->redirect(localIPURL); });
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SD, "/index.html", "text/html");
-  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SD, "/index.html", "text/html"); });
 
   server.serveStatic("/", SD, "/");
   server.begin();
@@ -107,11 +118,13 @@ void setUpWebServer() {
   Serial.println("Web server is up");
 }
 
-void setup() {
+void setup()
+{
   Serial.setTxBufferSize(1024);
-	Serial.begin(115200);
+  Serial.begin(115200);
 
-  while (!Serial);
+  while (!Serial)
+    ;
 
   // do some power savings
   setCpuFrequencyMhz(80);
@@ -128,7 +141,8 @@ void setup() {
   Serial.println("Boot complete.");
 }
 
-void loop() {
-	dnsServer.processNextRequest();
-	delay(50);
+void loop()
+{
+  dnsServer.processNextRequest();
+  delay(50);
 }
